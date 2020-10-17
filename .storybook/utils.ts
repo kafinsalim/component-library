3a4da922f46075d80e37af474966d4f098d6ddcf
arrlancores <arrlancore@gmail.com>
2020-10-17 13:34:38 +0700

    Add global style

--- .storybook/utils.ts
+++ .storybook/utils.ts
@@ -1,6 +1,8 @@
 import { jsxDecorator } from 'storybook-addon-jsx';
 import { withKnobs } from '@storybook/addon-knobs';
+import { globalStyle } from '../components/shared/styles';
 
-const decorators = [jsxDecorator, withKnobs];
+
+const decorators = [jsxDecorator, withKnobs, globalStyle];
 
 export default decorators;
