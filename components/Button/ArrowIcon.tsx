79113ef69c37f2b666c8826028f42a31cba7645c
arrlancores <arrlancore@gmail.com>
2020-10-20 22:40:31 +0700

    feat: Add arrow icon

---
+++ components/Button/ArrowIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+export function ArrowRight(props: any) {
+  return (
+    <svg id="arrow_right" width={19} height={14} viewBox="0 0 19 14" fill="none" {...props}>
+      <path
+        d="M.4 6.991l16.298-.03m0 0L11.252 1.61m5.447 5.351l-5.416 5.907"
+        stroke="#fff"
+        strokeWidth={2}
+      />
+    </svg>
+  )
+}
+
